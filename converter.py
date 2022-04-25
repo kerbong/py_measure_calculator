f9de56b241fa74148a843e838f0eec27393e930f
kerbong <kerbong@gmail.com>
2022-04-26 00:20:54 +0900

    add temperature calculator

--- converter.py
+++ converter.py
@@ -1,9 +1,24 @@
 from tkinter import *
 
 window = Tk()
-window.title("몇 mile ??")
+window.title("몇 mile ?? 화씨온도 ??")
 window.minsize(width=400, height=100)
 window.config(background="white", padx=20, pady=20)
+
+#단위 변환 기능
+def change_measure(): 
+    if measure_button.cget("text") == "섭씨<=>화씨":
+        from_label.config(text="℃")
+        to_label.config(text="℉")
+        measure_button.config(text="miles<=>km")
+    else:
+        from_label.config(text="miles")
+        to_label.config(text="km")
+        measure_button.config(text="섭씨<=>화씨")
+
+#단위 변환 버튼
+measure_button = Button(text="섭씨<=>화씨", command=change_measure)
+measure_button.grid(row=0, column=0)
 
 #숫자 입력창
 input_num = Entry(bg="orange", width=10)
@@ -27,14 +42,14 @@ to_label = Label(text="km", bg="white")
 to_label.grid(row=2, column=2)
 
 
-#from to 바꾸기 기능
+#단위 바꾸기 기능
 def change_measure():
     origin_from = from_label.cget("text")
     origin_to = to_label.cget("text")
     from_label.config(text=origin_to)
     to_label.config(text=origin_from)
 
-#from to 바꾸기 버튼
+#단위 바꾸기 버튼
 change_btn = Button(text="단위바꾸기", command=change_measure)
 change_btn.grid(row=2, column=3)
 
@@ -44,23 +59,19 @@ def calcul():
     from_measure = float(input_num.get())
     if from_label.cget("text") == "miles":
         result_label.config(text=round(from_measure * 1.609344, 2))
-    else:
+    elif from_label.cget("text") == "km":
         result_label.config(text=round(from_measure * 0.621371, 2))
+    elif from_label.cget("text") == "℃":
+        result_label.config(text=round((from_measure * 9 / 5) + 32, 1))
+    else:
+        result_label.config(text=round((from_measure-32)*5/9, 1))
         
+        
+
 #계산 버튼
 cal_button = Button(text="계산", command=calcul)
 cal_button.config(padx=3, pady=3)
 cal_button.grid(row=4, column=2)
-
-
-
-
-
-
-
-
-
-
 
 
 
